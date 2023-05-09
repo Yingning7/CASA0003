9571c0bee3e48a930dd0cfb3ae9f3365d603b0e0
Yingning7 <414891445@qq.com>
2023-05-09 22:31:15 +0100

    semi-final

--- pages/1_Tab_1.py
+++ pages/1_Tab_1.py
@@ -18,13 +18,10 @@ st.title('UK Night Time Economy 2012-2022')
 
 data = pd.read_csv('https://raw.githubusercontent.com/Yingning7/CASA0003/master/data/uknights_complete.csv')
 
-data['type'] = data['type'].astype(str)
-
-
 left, right = st.columns([1, 2])
 with left:
-    selected_year = st.select_slider('year', tuple(np.sort(data['year'].unique())))
-    selected_type = st.selectbox('type', np.sort(data['type'].unique()).tolist())
+    selected_year = st.select_slider('Year', tuple(np.sort(data['year'].unique())))
+    selected_type = st.selectbox('Type', np.sort(data['type'].unique()).tolist())
 
 data_filtered = data.copy()
 data_filtered = data_filtered.loc[data['year'] == selected_year]
@@ -37,7 +34,7 @@ map_fig = px.scatter_mapbox(
     color='type',
     size='num_employees',
     hover_name='region',
-    size_max=(data_filtered['num_employees'].max())/1000,
+    size_max=(data_filtered['num_employees'].max()) / 1000,
     zoom=4.65,
     height=700,
     center={'lat': 55.58316, 'lon': -3.833221},
@@ -48,8 +45,19 @@ map_fig = px.scatter_mapbox(
         'Health & Personal Social Services (support)': 'purple'
     }
 )
-map_fig.update_layout(mapbox_style='dark', mapbox_accesstoken='pk.eyJ1IjoiY2NlNzciLCJhIjoiY2xkMWt6amZzMHF6bjNvcGgwbHlvZzl1ZSJ9.UMSqMJzNGQELF2xTwuZLUw')
+map_fig.update_layout(
+    mapbox_style='dark',
+    mapbox_accesstoken='pk.eyJ1IjoiY2NlNzciLCJhIjoiY2xkMWt6amZzMHF6bjNvcGgwbHlvZzl1ZSJ9.UMSqMJzNGQELF2xTwuZLUw'
+)
 map_fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
+map_fig.update_layout(
+    legend={
+        'yanchor': 'top',
+        'y': 0.99,
+        'xanchor': 'left',
+        'x': 0.01
+    }
+)
 
 with right:
     st.plotly_chart(map_fig, use_container_width=True)
