9c3eef475c8b805a19178964074ed4d843db9d8f
Yingning7 <414891445@qq.com>
2023-05-10 21:52:02 +0100

    update

--- pages/1_Tab_1.py
+++ pages/1_Tab_1.py
@@ -16,56 +16,76 @@ st.set_page_config(
 
 st.title('UK Night Time Economy 2012-2022')
 
-data = pd.read_csv('https://raw.githubusercontent.com/Yingning7/CASA0003/master/data/uknights_complete.csv')
+data = pd.read_csv('./data/uknights_complete.csv')
 
 left, right = st.columns([1, 2])
 with left:
     selected_year = st.select_slider('Year', tuple(np.sort(data['year'].unique())))
-    selected_group = st.selectbox('Grouper', ['All', 'Primary Type', 'Secondary Type'])
-    if selected_group == 'Primary Type':
+    selected_group = st.selectbox('Grouper', ['All', 'Primary Group', 'Secondary Group'])
+    if selected_group == 'Primary Group':
         selected_type = st.selectbox('Primary Type', sorted(data['primary_type'].unique().tolist()))
-    elif selected_group == 'Secondary Type':
-        selected_type = st.select('Secondary Type', sorted(data['secondary_type'].unique().tolist()))
+    elif selected_group == 'Secondary Group':
+        selected_type = st.selectbox('Secondary Type', sorted(data['secondary_type'].unique().tolist()))
     elif selected_group == 'All':
         selected_type = 'All'
 
-print(selected_year, selected_group, selected_type)
+data_filtered = data.copy()
+data_filtered = data_filtered.loc[data['year'] == selected_year]
+if selected_group == 'All':
+    data_filtered = data_filtered.groupby(['year', 'region', 'lat', 'lon'])['num_employees'].sum().reset_index()
+elif selected_group == 'Primary Group':
+    data_filtered = data_filtered.groupby(['year', 'region', 'lat', 'lon', 'primary_type'])['num_employees'].sum().reset_index()
+    data_filtered = data_filtered.loc[data_filtered['primary_type'] == selected_type]
+elif selected_group == 'Secondary Group':
+    data_filtered = data_filtered = data_filtered.loc[data_filtered['secondary_type'] == selected_type]
+data_filtered['use_type'] = selected_type
 
-# data_filtered = data.copy()
-# data_filtered = data_filtered.loc[data['year'] == selected_year]
-# data_filtered = data_filtered.loc[data_filtered[selected_group] == selected_type]
-#
-# map_fig = px.scatter_mapbox(
-#     data_filtered,
-#     lat='lat',
-#     lon='lon',
-#     color='type',
-#     size='num_employees',
-#     hover_name='region',
-#     size_max=(data_filtered['num_employees'].max()) / 5000,
-#     zoom=4.65,
-#     height=700,
-#     center={'lat': 55.58316, 'lon': -3.833221},
-#     color_discrete_map={
-#         'Culture & Leisure': 'red',
-#         'Culture & Leisure (support)': 'green',
-#         'Health & Personal Social Services': 'blue',
-#         'Health & Personal Social Services (support)': 'purple'
-#     }
-# )
-# map_fig.update_layout(
-#     mapbox_style='dark',
-#     mapbox_accesstoken='pk.eyJ1IjoiY2NlNzciLCJhIjoiY2xkMWt6amZzMHF6bjNvcGgwbHlvZzl1ZSJ9.UMSqMJzNGQELF2xTwuZLUw'
-# )
-# map_fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
-# map_fig.update_layout(
-#     legend={
-#         'yanchor': 'top',
-#         'y': 0.99,
-#         'xanchor': 'left',
-#         'x': 0.01
-#     }
-# )
-#
-# with right:
-#     st.plotly_chart(map_fig, use_container_width=True)
+region_lat_lon = data[['region', 'lat', 'lon']].drop_duplicates().copy()
+with left:
+    locate = st.selectbox('Locate', ['Whole UK'] + sorted(region_lat_lon['region'].unique().tolist()))
+if locate == 'Whole UK':
+    centre = {'lat': 55.58316, 'lon': -3.833221}
+    zoom = 4.65
+else:
+    centre = {
+        'lat': region_lat_lon.loc[region_lat_lon['region'] == locate, 'lat'].item(),
+        'lon': region_lat_lon.loc[region_lat_lon['region'] == locate, 'lon'].item()
+    }
+    zoom = 10
+
+map_fig = px.scatter_mapbox(
+    data_filtered,
+    lat='lat',
+    lon='lon',
+    color='use_type',
+    size='num_employees',
+    hover_name='region',
+    size_max=(data_filtered['num_employees'].max()) / 5000,
+    zoom=zoom,
+    height=700,
+    center=centre,
+    color_discrete_map={
+        'Culture & Leisure (native)': 'red',
+        'Culture & Leisure (support)': 'green',
+        'Health & Personal Social Services (native)': 'blue',
+        'Health & Personal Social Services (support)': 'purple',
+        'Culture & Leisure': 'red',
+        'Health & Personal Social Services': 'blue'
+    }
+)
+map_fig.update_layout(
+    mapbox_style='dark',
+    mapbox_accesstoken='pk.eyJ1IjoiY2NlNzciLCJhIjoiY2xkMWt6amZzMHF6bjNvcGgwbHlvZzl1ZSJ9.UMSqMJzNGQELF2xTwuZLUw'
+)
+map_fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
+map_fig.update_layout(
+    legend={
+        'yanchor': 'top',
+        'y': 0.99,
+        'xanchor': 'left',
+        'x': 0.01
+    }
+)
+
+with right:
+    st.plotly_chart(map_fig, use_container_width=True)
