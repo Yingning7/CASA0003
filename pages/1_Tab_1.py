871f5968e239e48c809fa25ea8f2a3855161441e
Yingning7 <414891445@qq.com>
2023-05-10 20:57:36 +0100

    update

--- pages/1_Tab_1.py
+++ pages/1_Tab_1.py
@@ -21,43 +21,51 @@ data = pd.read_csv('https://raw.githubusercontent.com/Yingning7/CASA0003/master/
 left, right = st.columns([1, 2])
 with left:
     selected_year = st.select_slider('Year', tuple(np.sort(data['year'].unique())))
-    selected_type = st.selectbox('Type', np.sort(data['type'].unique()).tolist())
+    selected_group = st.selectbox('Grouper', ['All', 'Primary Type', 'Secondary Type'])
+    if selected_group == 'Primary Type':
+        selected_type = st.selectbox('Primary Type', sorted(data['primary_type'].unique().tolist()))
+    elif selected_group == 'Secondary Type':
+        selected_type = st.select('Secondary Type', sorted(data['secondary_type'].unique().tolist()))
+    elif selected_group == 'All':
+        selected_type = 'All'
 
-data_filtered = data.copy()
-data_filtered = data_filtered.loc[data['year'] == selected_year]
-data_filtered = data_filtered.loc[data_filtered['type'] == selected_type]
+print(selected_year, selected_group, selected_type)
 
-map_fig = px.scatter_mapbox(
-    data_filtered,
-    lat='lat',
-    lon='lon',
-    color='type',
-    size='num_employees',
-    hover_name='region',
-    size_max=(data_filtered['num_employees'].max()) / 5000,
-    zoom=4.65,
-    height=700,
-    center={'lat': 55.58316, 'lon': -3.833221},
-    color_discrete_map={
-        'Culture & Leisure': 'red',
-        'Culture & Leisure (support)': 'green',
-        'Health & Personal Social Services': 'blue',
-        'Health & Personal Social Services (support)': 'purple'
-    }
-)
-map_fig.update_layout(
-    mapbox_style='dark',
-    mapbox_accesstoken='pk.eyJ1IjoiY2NlNzciLCJhIjoiY2xkMWt6amZzMHF6bjNvcGgwbHlvZzl1ZSJ9.UMSqMJzNGQELF2xTwuZLUw'
-)
-map_fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
-map_fig.update_layout(
-    legend={
-        'yanchor': 'top',
-        'y': 0.99,
-        'xanchor': 'left',
-        'x': 0.01
-    }
-)
-
-with right:
-    st.plotly_chart(map_fig, use_container_width=True)
+# data_filtered = data.copy()
+# data_filtered = data_filtered.loc[data['year'] == selected_year]
+# data_filtered = data_filtered.loc[data_filtered[selected_group] == selected_type]
+#
+# map_fig = px.scatter_mapbox(
+#     data_filtered,
+#     lat='lat',
+#     lon='lon',
+#     color='type',
+#     size='num_employees',
+#     hover_name='region',
+#     size_max=(data_filtered['num_employees'].max()) / 5000,
+#     zoom=4.65,
+#     height=700,
+#     center={'lat': 55.58316, 'lon': -3.833221},
+#     color_discrete_map={
+#         'Culture & Leisure': 'red',
+#         'Culture & Leisure (support)': 'green',
+#         'Health & Personal Social Services': 'blue',
+#         'Health & Personal Social Services (support)': 'purple'
+#     }
+# )
+# map_fig.update_layout(
+#     mapbox_style='dark',
+#     mapbox_accesstoken='pk.eyJ1IjoiY2NlNzciLCJhIjoiY2xkMWt6amZzMHF6bjNvcGgwbHlvZzl1ZSJ9.UMSqMJzNGQELF2xTwuZLUw'
+# )
+# map_fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
+# map_fig.update_layout(
+#     legend={
+#         'yanchor': 'top',
+#         'y': 0.99,
+#         'xanchor': 'left',
+#         'x': 0.01
+#     }
+# )
+#
+# with right:
+#     st.plotly_chart(map_fig, use_container_width=True)
