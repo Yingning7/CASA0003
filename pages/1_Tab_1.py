cc2c95ef36a140afda3a5011c4339b345da3847b
Yingning7 <414891445@qq.com>
2023-05-10 21:52:45 +0100

    update

--- pages/1_Tab_1.py
+++ pages/1_Tab_1.py
@@ -16,7 +16,7 @@ st.set_page_config(
 
 st.title('UK Night Time Economy 2012-2022')
 
-data = pd.read_csv('./data/uknights_complete.csv')
+data = pd.read_csv('https://raw.githubusercontent.com/Yingning7/CASA0003/master/data/uknights_complete.csv')
 
 left, right = st.columns([1, 2])
 with left:
