02a5aaf6530ba00de73ff7997e86b3f74bbf49f2
Yingning7 <414891445@qq.com>
2023-05-14 22:03:29 +0100

    update

--- Home.py
+++ Home.py
@@ -1,7 +1,4 @@
-import plotly.express as px
 import streamlit as st
-import pandas as pd
-import numpy as np
 
 st.set_page_config(
     page_title='CASA0003 Visualisation - Home',
--- pages/3_Tab_3.py
+++ pages/3_Tab_3.py
@@ -12,8 +12,3 @@ st.set_page_config(
 )
 
 st.title('This is Tab 3')
-
-with open('./pages/Workplaces.html', mode='r') as fp:
-    html_str = fp.read()
-
-st.components.v1.html(html_str, width=500, height=500)
