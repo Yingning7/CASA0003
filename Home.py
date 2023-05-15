db57feaa15089fd6d0e4799b1b9ab4fc91316792
Yingning7 <414891445@qq.com>
2023-05-15 22:22:08 +0100

    update

--- Home.py
+++ Home.py
@@ -12,3 +12,16 @@ st.set_page_config(
 )
 
 st.title('CASA0003 Visualisation')
+
+st.markdown(
+     f"""
+     <style>
+     .stApp {{
+         background-image: url("https://i.pinimg.com/originals/04/4e/68/044e689920b539251bfe223c4a4a3dd1.jpg");
+         background-attachment: fixed;
+         background-size: cover
+     }}
+     </style>
+     """,
+     unsafe_allow_html=True
+ )
